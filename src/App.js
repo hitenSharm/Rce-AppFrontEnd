a9e2510ba325638601113f51d7f281bba31e4988
hitenSharma <hiten.sharm1708@gmail.com>
2020-12-16 16:32:57 +0530

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+import Giveback from './OP';
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/ext-language_tools"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  state={
+    code:''
+  }
+
+  handleRun = () =>{
+    console.log(this.state.code)
+  }
+
+  
+  handleChange = (newVal) =>{
+    this.setState({
+      code:newVal
+    })
+  }
+
+  render(){
+    return(
+      <div>
+      <AceEditor     
+      mode="python"
+      width='1000px'
+      height='500px'
+      theme="monokai"
+      onChange={this.handleChange}
+      editorProps={{ $blockScrolling: true }}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true
+      }}
+      /> 
+      <button onClick={this.handleRun}>RUN CODE</button>
+       <Giveback/>  
+       
     </div>
-  );
+    );
+  }
 }
 
 export default App;
